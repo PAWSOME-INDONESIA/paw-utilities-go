44bba1f3a1c05ff19ecce9b2416a14b59890ecc2
Paulus Robin <paulus.robin@hotmail.com>
2019-10-14 23:06:19 +0700

    Remove close

--- messaging/kafka/base.go
+++ messaging/kafka/base.go
@@ -171,8 +171,6 @@ func (k *kafka) PublishWithContext(ctx context.Context, topic, message string) e
 	k.mu.Unlock()
 
 	w := k.writers[topic]
-	defer func() { _ = w.Close() }()
-
 	if err := w.WriteMessages(context.Background(), kfk.Message{Value: []byte(message)}); err != nil {
 		k.log.Error(err)
 		return errors.Wrapf(err, "failed to publish message on topic %s", topic)
